6196ecbf91ef1381efe760d4c7236c0fd94ac0bf
Leonardo9507 <leonardo.soto2@upr.edu>
2026-03-13 16:23:44 -0400

    Fixed some minor logical issues

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -11,15 +11,10 @@ class DyEnemy : public Enemy {
     public:
         DyEnemy(float x, float y) : Enemy(x, y) { 
             this->cooldown = GetRandomValue(90, 300);
-<<<<<<< HEAD
-            this->health = 1;
-        }
-=======
             this->health = 1; 
             this->scoreIncrement = 75;
->>>>>>> 5faa39bcc5270ed0a8ec9d12af2b51e27f96d674
-
         }
+
         int getScoreIncrement(){return this->scoreIncrement;}
         void draw() override;
         void update(std::pair<float, float> pos, HitBox target) override;
