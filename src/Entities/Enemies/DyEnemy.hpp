2533e91a423cdb618083c13312e7401ce72f41ec
Leonardo9507 <leonardo.soto2@upr.edu>
2026-03-13 16:46:04 -0400

    DyEnemy Rectangle has been successfully implemented

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -13,6 +13,12 @@ class DyEnemy : public Enemy {
             this->cooldown = GetRandomValue(90, 300);
             this->health = 1; 
             this->scoreIncrement = 75;
+
+            if (GetRandomValue(0,1) == 0){
+                this->texture = Rectangle{2, 128, 13, 14};
+            } else {
+                this->texture = Rectangle{2, 147, 13, 13};
+            }
         }
 
         int getScoreIncrement(){return this->scoreIncrement;}
